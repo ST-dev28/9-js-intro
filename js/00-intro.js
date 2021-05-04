03b7bafd6d6921a809d10e0ef1db0d35ce8a8959
SandraT <sndrbltr@gmail.com>
2021-05-04 15:56:00 +0000

    Initial commit

---
+++ js/00-intro.js
@@ -0,0 +1,18 @@
+// single -line comment
+
+/*
+multi 
+line 
+comment
+*/
+
+/*
+KONCEPTAI:
+1) kintamieji - isimena informacija
+2) salygos sakiniai - tarpusavio palyginimas
+3) ciklai - darbas su pasikartojancio formato informacija, 
+pasikartojanciu procesu valdymas
+4) funkcijos - uzdara logika, kuria galima perpanaudoti pagal poreiki (pvz vidurkio paskaiciavimas)
+5) ivykiai - uzsikrove puslapis, paspaude klaviaturos mygtuka, pradejo siusti faila, issiunte uzklausa it t.t.
+6) oop (object oriented programming) = funkcijos - tai tik skonio reikalas
+*/
