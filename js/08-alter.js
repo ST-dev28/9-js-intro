7cd3de6b97108f22951e0d89a5e1dcf58597522d
SandraT <sndrbltr@gmail.com>
2021-05-06 16:37:25 +0000

    Alter & ternary & switch

--- js/08-alter.js
+++ js/08-alter.js
@@ -4,6 +4,12 @@ alternuojanti eilute
 
 const list = [10, 2, 8, 4, 6];
 
-let sum = 0;
+let suma = 0;
 
+suma += list[0];
+suma -= list[1];
+suma += list[2];
+suma -= list[3];
+suma += list[4];
 
+console.log(suma);
