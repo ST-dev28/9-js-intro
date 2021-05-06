463d59e6219197fdff7eee91d0fcbafb7cd6ea8d
SandraT <sndrbltr@gmail.com>
2021-05-06 08:49:51 +0000

    If

--- js/06-if.js
+++ js/06-if.js
@@ -10,9 +10,9 @@ naudotini: >, <, >=, <=, ===, !==
 SABLONAI:
 if () {}
 if () {} else {}
-if () {} else If () {}
-if () {} else if () {} ... else if () {}
-if () {} else if () {} ... else if () {} else {}
+if () {} else if () {}
+if () {} else if () {} ...  else if () {}
+if () {} else if () {} ...  else if () {} else {}
 
 */
 
@@ -52,19 +52,62 @@ if (c == d) {
 }
     //
 
-
-    const akys = 'pilkos';
+    const akys = 'melynos';
 
     if (akys === 'melynos') {
         console.log('Melynakis');
-} else if (akys === 'zalios') {
-    console.log('Zaliaakis');
-} else if (akys === 'rudos') {
-    console.log('Rudaakis');
-} else {
-    console.log('Neatpazinta akiu spalva ...');
-}
-
-console.log('----------------');
-
-const parosMetas = 'rytas';
+    } else if (akys === 'zalios') {
+        console.log('Zaliaakis');
+    } else if (akys === 'rudos') {
+        console.log('Rudaakis');
+    } else {
+        console.log('Neatpazinta akiu spalva... 🤔');
+    }
+    
+    
+    const plaukai = 'rudos';
+    
+    if (plaukai === 'melynos') {
+        console.log('Melynaplaukis');
+    }
+    
+    if (plaukai === 'zalios') {
+        console.log('Zaliaplaukis');
+    }
+    
+    if (plaukai === 'rudos') {
+        console.log('Rudaplaukis');
+    } else {
+        console.log('Plauku spalva nera ruda... 🤔');
+    }
+    
+    console.log('--------------------');
+    
+    const parosMetas = 'rytas';
+    const arLyja = false;
+    
+    if (parosMetas === 'rytas') {
+        if (arLyja) {
+            console.log('Labas rytas, nepamirsk skecio!');
+        } else {
+            console.log('Labas rytas!');
+        }
+    } else {
+        if (parosMetas === 'diena') {
+            if (arLyja) {
+                console.log('Labas diena, nepamirsk skecio!');
+            } else {
+                console.log('Labas diena!');
+            }
+        } else {
+            if (parosMetas === 'vakaras') {
+                if (arLyja) {
+                    console.log('Labas vakaras, nepamirsk skecio!');
+                } else {
+                    console.log('Labas vakaras!');
+                }
+            } else {
+                console.log('Neatpazintas paros metas');
+            }
+        }
+    }
