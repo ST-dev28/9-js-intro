09405f680fd101b34ed2cd1c0570c652abe96ff7
SandraT <sndrbltr@gmail.com>
2021-05-07 16:26:30 +0000

    Hi, sum, day, car

---
+++ js/13-sum.js
@@ -0,0 +1,24 @@
+// pirma apsirasome, ka naudosim veliau:
+function sumavimas (pirmasSkaicius, antrasSkaicius) {
+    console.log('Iskviestas sumavimas!!!');
+    console.log('Pirmas:', pirmasSkaicius);
+    console.log('Antras:', antrasSkaicius);
+    return pirmasSkaicius + antrasSkaicius;
+
+}
+
+const s1 = sumavimas(8, 5);
+console.log(s1);
+
+const s2 = sumavimas(99, 102);
+console.log(s2);
+
+const s3 = sumavimas(77, 777);
+console.log(s3);
+
+
+const a = 6;
+const b = 66;
+
+const c = a + b;
+console.log(c);
