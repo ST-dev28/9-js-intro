1c16886ddefd7114c9a58f8f04c9f77fff9dc1ba
SandraT <sndrbltr@gmail.com>
2021-05-10 17:16:46 +0000

    Bricks kitoks sprendimas

--- random/bricks.js
+++ random/bricks.js
@@ -5,14 +5,14 @@ UZDUOTIS:
 - sienos neturi angu ir / ar skyliu
 - gaunam sienos auksti, kuris visur yra vienodas
 - turim plytos dydi (aukstis, plotis)
-Reikia suznoti, keliu plytu reikes, jei ju skaldyti negalima
+Reikia suznoti, keliu plytu reikes, jei ju skaldyti negalima. 
 matavimo vienetai metrai
 */
-
+/*
 function bricks(sienuPlociai, sienosAukstis,plytosAukstis, plytosPlotis) {
     let bendrasSienuPlotis = 0;
 
-    for (let i =0; i < sienuPlociai.length; i++) {
+    for (let i = 0; i < sienuPlociai.length; i++) {
         bendrasSienuPlotis += sienuPlociai[i];
     } 
     const plytuPlotyje = bendrasSienuPlotis / plytosPlotis;
@@ -20,7 +20,26 @@ function bricks(sienuPlociai, sienosAukstis,plytosAukstis, plytosPlotis) {
     
 return plytuPlotyje * plytuAukstyje; 
 } 
+*/
 
+
+ // ARBA: 
+
+    function bricks(sienuPlociai, sienosAukstis,plytosAukstis, plytosPlotis) {
+        let bendrasSienuPlotis = 0;
+
+        for (let i = 0; i < sienuPlociai.length; i++) {
+           const sienosPlotis = sienuPlociai[i];
+           bendrasSienuPlotis += sienosPlotis;
+    }
+    const plytuPlotyje = Math.ceil(bendrasSienuPlotis / plytosPlotis);
+    const plytuAukstyje = Math.ceil(sienosAukstis / plytosAukstis);
+
+    const plytuKiekis = plytuPlotyje * plytuAukstyje;
+
+    return plytuKiekis;
+
+    }
 
 console.log(bricks([1, 2], 1, 0.1, 0.2), '->', 150);
 console.log(bricks([1, 1, 1], 1, 0.1, 0.2), '->', 150);
